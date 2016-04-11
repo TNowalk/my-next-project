3172732a9d0f18c63ee17c3994d0a0a52a57bfa0
Trevor Nowalk <trevor.nowalk@twcable.com>
2016-04-11 00:28:36 -0400

    Adding milestone 2 content

---
+++ api/comment/comment.model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+  text: String,
+  upvotes: {
+    type: Number,
+    default: 0
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project'
+  },
+  creator: String
+});
+
+mongoose.model('Comment', CommentSchema);
---
+++ api/project/project.model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var ProjectSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  upvotes: {
+    type: Number,
+    default: 0
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  creator: String
+});
+
+mongoose.model('Project', ProjectSchema);
